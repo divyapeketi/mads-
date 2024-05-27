8d315ee3618ed1a0d8efcf77777f0a29fb0a4965
divyapeketi <144924955+divyapeketi@users.noreply.github.com>
2024-05-27 13:03:31 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,29 +1,66 @@
-noun = input('Choose a noun: ')
+import random
 
-p_noun = input('Choose a plural noun: ')
 
-noun2 = input('Choose a noun: ')
+def show_score(attempts_list):
+    if not attempts_list:
+        print('There is currently no best score,'
+              ' it\'s yours for the taking!')
 
-place = input('Name a place: ')
+    else:
+        print(f'The current best score is'
+              f' {min(attempts_list)} attempts')
 
-adjective = input('Choose an adjective (Describing word): ')
 
-noun3 = input('Choose a noun: ')
+def start_game():
+    attempts = 0
+    rand_num = random.randint(1, 10)
+    attempts_list = []
 
-# Print a story from the user input
+    print('Hello traveler! Welcome to the game of guesses!')
+    player_name = input('What is your name? ')
+    wanna_play = input(
+        f'Hi, {player_name}, would you like to play '
+        f'the guessing game? (Enter Yes/No): ')
 
-print('------------------------------------------')
+    if wanna_play.lower() != 'yes':
+        print('That\'s cool, Thanks!')
+        exit()
+    else:
+        show_score(attempts_list)
 
-print('Be kind to your', noun, '- footed', p_noun)
+    while wanna_play.lower() == 'yes':
+        try:
+            guess = int(input('Pick a number between 1 and 10: '))
+            if guess < 1 or guess > 10:
+                raise ValueError(
+                    'Please guess a number within the given range')
 
-print('For a duck may be somebody\'s', noun2, ',')
+            attempts += 1
 
-print('Be kind to your', p_noun, 'in', place)
+            if guess == rand_num:
+                attempts_list.append(attempts)
+                print('Nice! You got it!')
+                print(f'It took you {attempts} attempts')
+                wanna_play = input(
+                    'Would you like to play again? (Enter Yes/No): ')
+                if wanna_play.lower() != 'yes':
+                    print('That\'s cool, have a good one!')
+                    break
+                else:
+                    attempts = 0
+                    rand_num = random.randint(1, 10)
+                    show_score(attempts_list)
+                    continue
+            else:
+                if guess > rand_num:
+                    print('It\'s lower')
+                elif guess < rand_num:
+                    print('It\'s higher')
 
-print('Where the weather is always', adjective, '. \n')
+        except ValueError as err:
+            print('Oh no!, that is not a valid value. Try again...')
+            print(err)
 
-print('You may think that is this the', noun3, ',')
 
-print('Well it is.')
-
-print('------------------------------------------')
+if _name_ == '_main_':
+    start_game()
